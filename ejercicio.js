d74df7636c279ede0c9a5e88fe6041ab846b8687
mariaabi82 <maria.abi1982@gmail.com>
2019-11-22 18:27:56 -0300

    empiezo a escribir la parte de obtener

--- ejercicio.js
+++ ejercicio.js
@@ -21,26 +21,23 @@ if (menu === "AGREGAR") {
     nombre = prompt('Ingresa tu nombre');
     telefono = prompt("Número");
     email = prompt("Email");
-};
 
-let datosNuevoUsuario = [`${id}`, nombre , telefono , email];
-/*alert(`Los datos ingresados fueron: ${datosNuevoUsuario}`);*/
-let personas=prompt(`Los datos ingresados fueron
+
+  let datosNuevoUsuario = [`${id}`, nombre , telefono , email];
+  /*alert(`Los datos ingresados fueron: ${datosNuevoUsuario}`);*/
+  let personas=prompt(`Los datos ingresados fueron
     NOMBRE: ${nombre}
     TELEFONO: ${telefono}
     EMAIL: ${email}
     Confirma los cambios?`)
 
-if(personas ==="SI"){
+  if(personas ==="SI"){
      listaUsuarios.push(datosNuevoUsuario);
     alert("¡Operación realizada exitosamente!")
-} else{
+  } else{
     alert("¡Operación cancelada!")
+  };
 };
-
-
-
-
 
 
 /*let personasAgregadas = [
@@ -51,6 +48,18 @@ if(personas ==="SI"){
 
 //id a la longitud del array//*/
 
+let menu2 = prompt(`Buscar por ID, Nombre, Celular o Email?`)
 
+let menu3 = prompt(`Ingresar el valor a buscar`)
 
-
+if (menu === "OBTENER") {
+    if (menu2 === "ID") {
+        menu3 
+        for (let i = menu3; i = listaUsuarios.length; i++) {
+            console.log(listaUsuarios[i])
+            alert(`El usuario encontrado es el siguiente:
+            ${id} ${nombre} ${telefono} ${email}`)
+            
+        }
+    }
+} 
