2175669a62535e7adfd7cc5b1d22b5c218611853
malumart <malu.martin92@hotmail.com>
2019-11-25 13:36:38 -0300

    Agregue accion ELIMINAR

--- ejercicio.js
+++ ejercicio.js
@@ -1,107 +1,137 @@
 let listaUsuarios = [
-[0, "Carla", "1545628984", "carla@gmail.com"],
-[1, "Pedro", "1545251245", "pedro@gmail.com"],
-[2, "Lucas", "1523357849", "lucas@gmail.com"],
-[3, "Ana", "15789456", "ana@gmail.com"], 
-];
-
-let id = listaUsuarios.length;
-let nombre = [];
-let telefono = [];
-let email = [];
-let accion;
-let volverAlMenuPrincipal = "SI";
-
-while (volverAlMenuPrincipal == "SI"){
-accion = prompt(`✅ Seleccione una operacion 
---------------------------
-➡️ [AGREGAR] un usuario 👤
-➡️ [OBTENER] un usuario 🔎
-➡️ [LISTAR] todos los usuarios 📄
-➡️ [MODIFICAR] un usuario 📝
-➡️ [ELIMINAR] un usuario ❌ 
-➡️ [SALIR] del programa 👋`);
-
-if (accion == "AGREGAR"){   
-    let repetirOperacion = "SI";
-
-    while (repetirOperacion == 'SI'){
-    nombre= prompt(" 📓 Ingrese el nombre del usuario");
-    telefono= prompt(" 📞 Ingrese el teléfono del usuario");
-    email = prompt(" 📫 Ingrese email del usuario");
-    let nuevoUsuario =[id, nombre, telefono, email];
-
-    let respuesta = prompt(`Los datos del usuario ingresado son:
-    NOMBRE: ${name} 
-    TELEFONO: ${telephone} 
-    EMAIL: ${email}
-    Desea confirmar?`);
-
-    if (respuesta =="SI"){
-        usuarios.push(nuevoUsuario);
-        alert("El usuario fue guardado exitosamente!")
-    }else{
-        alert("Los cambios no han sido guardados, se cancelo la operacion.");
+    [0, "Carla", "1545628984", "carla@gmail.com"],
+    [1, "Pedro", "1545251245", "pedro@gmail.com"],
+    [2, "Lucas", "1523357849", "lucas@gmail.com"],
+    [3, "Ana", "15789456", "ana@gmail.com"], 
+    ];
+    
+    let id = listaUsuarios.length;
+    let nombre = [];
+    let telefono = [];
+    let email = [];
+    let accion;
+    let volverAlMenuPrincipal = "SI";
+    
+    while (volverAlMenuPrincipal == "SI"){
+    accion = prompt(`✅ Seleccione una operacion 
+    --------------------------
+    ➡️ [AGREGAR] un usuario 👤
+    ➡️ [OBTENER] un usuario 🔎
+    ➡️ [LISTAR] todos los usuarios 📄
+    ➡️ [MODIFICAR] un usuario 📝
+    ➡️ [ELIMINAR] un usuario ❌ 
+    ➡️ [SALIR] del programa 👋`);
+    
+    if (accion == "AGREGAR"){   
+        let repetirOperacion = "SI";
+    
+        while (repetirOperacion == 'SI'){
+        nombre= prompt(" 📓 Ingrese el nombre del usuario");
+        telefono= prompt(" 📞 Ingrese el teléfono del usuario");
+        email = prompt(" 📫 Ingrese email del usuario");
+        let nuevoUsuario =[id, nombre, telefono, email];
+    
+        let respuesta = prompt(`Los datos del usuario ingresado son:
+        NOMBRE: ${name} 
+        TELEFONO: ${telephone} 
+        EMAIL: ${email}
+        Desea confirmar?`);
+    
+        if (respuesta =="SI"){
+            usuarios.push(nuevoUsuario);
+            alert("El usuario fue guardado exitosamente!")
+        }else{
+            alert("Los cambios no han sido guardados, se cancelo la operacion.");
+        }
+    
+        repetirOperacion = prompt(`Desea ingresar otro usuario?`);
+        }
+    
     }
-
-    repetirOperacion = prompt(`Desea ingresar otro usuario?`);
+    
+    if (accion == "OBTENER") {
+    
+        let repetirOperacion = "SI";
+        let usuarioEncontrado = false;
+        while (repetirOperacion == 'SI'){
+        let tipoDeDato = prompt(`Buscar por ID, Nombre, Celular o Email?`);
+        let valorABuscar = prompt(`Ingresar el valor a buscar`);
+    
+        if (tipoDeDato == "ID" || "Nombre" || "Celular" || "Email") {
+            for (let i = 0; i < listaUsuarios.length; i++) {
+                for (let j = 0; j < listaUsuarios[i].length; j++) {
+                    if (listaUsuarios[i][j] === valorABuscar) {
+                        alert(`El usuario encontrado es el siguiente:
+                    ${listaUsuarios[i]}`)
+                    usuarioEncontrado = true;
+                    } 
+                }
+            }
+    
+            if (usuarioEncontrado == false){ 
+                alert("No existe el usuario buscado");
+            }
+    
+        }
+    
+        repetirOperacion = prompt(`Desea realizar otra busqueda?`);
+    
     }
-
-}
-
-if (accion == "OBTENER") {
-
-    let repetirOperacion = "SI";
-    let usuarioEncontrado = false;
-    while (repetirOperacion == 'SI'){
-    let tipoDeDato = prompt(`Buscar por ID, Nombre, Celular o Email?`);
-    let valorABuscar = prompt(`Ingresar el valor a buscar`);
-
-    if (tipoDeDato == "ID" || "Nombre" || "Celular" || "Email") {
+    }
+    
+    if (accion == "LISTAR"){
+        let mostrarUsuarios = "";
         for (let i = 0; i < listaUsuarios.length; i++) {
-            for (let j = 0; j < listaUsuarios[i].length; j++) {
-                if (listaUsuarios[i][j] === valorABuscar) {
-                    alert(`El usuario encontrado es el siguiente:
-                ${listaUsuarios[i]}`)
-                usuarioEncontrado = true;
-                } 
+               mostrarUsuarios = mostrarUsuarios  + 
+               `
+                ID = ${listaUsuarios[i][0]}
+                NOMBRE = ${listaUsuarios[i][1]}
+                TELEFONO = ${listaUsuarios[i][2]}
+                MAIL = ${listaUsuarios[i][3]}
+                -----------
+                `;
+            }
+        alert (mostrarUsuarios);
+    
+    }
+    
+  
+    if (accion=="ELIMINAR"){
+        let repetirOperacion ="SI";
+        let usuarioEncontrado = false;
+        while( repetirOperacion =="SI"){
+        let usuarioAEliminar = prompt ("Indique el ID del usuario que desea eliminar");
+        for (i=0; i < listaUsuarios.length ; i++){
+            if (listaUsuarios[i][0] == usuarioAEliminar){
+                confirmarOperacion = prompt (`Los datos del usuario a eliminar son:
+                ID: ${listaUsuarios[i][0]}
+                NOMBRE: ${listaUsuarios[i][1]}
+                TELEFONO: ${listaUsuarios[i][2]}
+                MAIL: ${listaUsuarios[i][3]}
+                Desea confirmar? (SI-NO)
+                `)
+                if (confirmarOperacion == "SI"){
+                    listaUsuarios.splice(i,1);
+                    alert("La operacion ha sido realizada exitosamente");
+                } else{
+                    alert("La operacion ha sido cancelada");
+                }
+            usuarioEncontrado = "SI";
             }
         }
-
-        if (usuarioEncontrado == false){ 
-            alert("No existe el usuario buscado");
+        if (usuarioEncontrado == false){
+            alert("El usuario ingresado no existe.");
         }
-
+        repetirOperacion = prompt("Desea repetir la operacion? (SI-NO)");
+        }
     }
-
-    repetirOperacion = prompt(`Desea realizar otra busqueda?`);
-
-}
-}
-
-if (accion == "LISTAR"){
-    let mostrarUsuarios = "";
-    for (let i = 0; i < listaUsuarios.length; i++) {
-           mostrarUsuarios = mostrarUsuarios  + 
-           `
-            ID = ${listaUsuarios[i][0]}
-            NOMBRE = ${listaUsuarios[i][1]}
-            TELEFONO = ${listaUsuarios[i][2]}
-            MAIL = ${listaUsuarios[i][3]}
-            -----------
-            `;
-        }
-    alert (mostrarUsuarios);
-
-}
-
-
-
-if (accion =="SALIR"){
-    volverAlMenuPrincipal = "NO";
-}
-
-}
-
-console.log(usuarios);
-
+    
+    if (accion =="SALIR"){
+        volverAlMenuPrincipal = "NO";
+    }
+    
+    }
+    
+    console.log(listaUsuarios);
+    
+    
